4c73be31a900f39a09f96ecc639e8c5e988d4562
Sanyam Jain <xerosanyam@gmail.com>
2019-05-07 12:33:18 +0530

    add JTable

---
+++ components/JTable/JTable.vue
@@ -0,0 +1,25 @@
+<template>
+  <table>
+    <JTableRow
+      v-for="(values, index) in items"
+      :key="index"
+      :values="values"
+    />
+  </table>
+</template>
+
+<script>
+import JTableRow from '@xerosanyam/j-table-row/JTableRow'
+export default {
+  name: 'JTable',
+  props: {
+    items: {
+      type: Array,
+      required: true
+    }
+  },
+  components: {
+    JTableRow
+  }
+}
+</script>
